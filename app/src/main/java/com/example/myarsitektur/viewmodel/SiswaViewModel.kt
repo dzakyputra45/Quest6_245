1c626e565f36a65880b9b0e138b2a172f9ecaab4
dzakyputra45 <dzakypratama45@gmail.com>
2025-11-20 16:24:44 +0700

    Tambahkan semua Package yang diperlukan

---
+++ app/src/main/java/com/example/myarsitektur/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.myarsitektur.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.myarsitektur.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+
+        fun setSiswa(ls: MutableList<String>) {
+            _statusUI.update { statusSaatini ->
+                statusSaatini.copy(nama=ls(0), gender=ls(1), alamat=ls(2))
+            }
+        }
+    }
+
+private fun SiswaViewModel.ls(i: Int): String {
+    TODO("Not yet implemented")
+}
+
+
