0df367f66385e6e0c1d9d1dc4960674b8aaba67b
dzakyputra45 <dzakypratama45@gmail.com>
2025-11-20 16:30:59 +0700

    Tambahkan Package yang perlu

---
+++ app/src/main/java/com/example/myarsitektur/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myarsitektur
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.myarsitektur.ui.theme.MyArsitekturTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyArsitekturTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SiswaApp( modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
