1304a94e35687df9c4f34f18cb74eee5af88cb79
dzakyputra45 <dzakypratama45@gmail.com>
2025-11-20 16:38:44 +0700

    Padding pada horizontal divider ubah kw 25 dp

--- app/src/main/java/com/example/myarsitektur/view/uicontroller/FormSIswa.kt
+++ app/src/main/java/com/example/myarsitektur/view/uicontroller/FormSIswa.kt
@@ -74,7 +74,7 @@ fun FormSiswa(
 
             HorizontalDivider(
                 modifier = Modifier
-                    .padding(20.dp)
+                    .padding(25.dp)
                     .width(250.dp),
                 thickness = 1.dp,
                 color = Color.DarkGray
